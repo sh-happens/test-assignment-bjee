48f07a6c5ba18b4268e3b258a0deb8ea84e2d591
yerbol kosbayev <yerbolkosbayev@gmail.com>
2019-11-30 18:12:57 +0600

    alerts added

---
+++ src/actions/cards.js
@@ -0,0 +1,41 @@
+import { cardsConstants } from "../constants";
+import { cardsService } from "../services";
+import { alertActions } from "./";
+
+export const cardsActions = { getCards };
+
+function getCards(pageNumber) {
+  console.log("CARDSACTIONS");
+  return dispatch => {
+    dispatch(request(pageNumber));
+    dispatch(alertActions.success("Cards' request for page # " + pageNumber));
+    cardsService.getCards(pageNumber).then(
+      cards => {
+        dispatch(success(cards, pageNumber));
+        dispatch(
+          alertActions.success(
+            "Cards' request for page # " + pageNumber + " was successful"
+          )
+        );
+      },
+      error => {
+        dispatch(failure(error, pageNumber));
+        dispatch(
+          alertActions.error(
+            "Cards' request for page # " + pageNumber + " was unsuccessful"
+          )
+        );
+      }
+    );
+  };
+
+  function request(pageNumber) {
+    return { type: cardsConstants.GET_CARDS_REQUEST, pageNumber };
+  }
+  function success(cards, pageNumber) {
+    return { type: cardsConstants.GET_CARDS_SUCCESS, cards, pageNumber };
+  }
+  function failure(error, pageNumber) {
+    return { type: cardsConstants.GET_CARDS_FAILURE, error, pageNumber };
+  }
+}
