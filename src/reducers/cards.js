5c54218e8cad2ecdd9be0fbf8bf9c36fabc32ae5
yerbol kosbayev <yerbolkosbayev@gmail.com>
2019-11-30 18:29:17 +0600

    added sorting

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -6,17 +6,42 @@ export function cardsReducer(state = {}, action) {
       return {
         ...state
       };
+
     case cardsConstants.GET_CARDS_SUCCESS:
       return {
         ...state,
         cards: action.cards.message.tasks,
         pageNumber: action.pageNumber
       };
+
     case cardsConstants.GET_CARDS_FAILURE:
       return {
         ...state,
         error: action.error
       };
+
+    case cardsConstants.SORT_CARDS_REQUEST:
+      return {
+        ...state
+      };
+
+    case cardsConstants.SORT_CARDS_SUCCESS: {
+      console.log("action = ", action);
+      return {
+        ...state,
+        cards: action.cards.message.tasks,
+        sortField: action.sortField,
+        sortDirection: action.sortDirection
+      };
+    }
+
+    case cardsConstants.SORT_CARDS_FAILURE: {
+      return {
+        ...state,
+        error: action.error
+      };
+    }
+
     default:
       return state;
   }
